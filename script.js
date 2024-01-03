3e3c71214259992ca9a9049bac3127dc46fa9e55
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-01-03 23:03:46 +0530

    Created a Chatbot using HTML , CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+function talk() {
+  var know = {
+    "Who are you": "Hello, juhi here",
+    "How are you": "Good :)",
+    "Your favourite Cricket Team": "My favorite cricket team is Mumbai Indians",
+    ok: "Thank You So Much ",
+    Bye: "Okay! Will meet soon..",
+  };
+  var user = document.getElementById("userBox").value;
+  document.getElementById("chatLog").innerHTML = user + "<br>";
+  if (user in know) {
+    document.getElementById("chatLog").innerHTML = know[user] + "<br>";
+  } else {
+    document.getElementById("chatLog").innerHTML =
+      "Sorry,I didn't understand <br>";
+  }
+}
